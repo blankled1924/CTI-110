b3c71c1394c6feeaa048a4685d8b083dc54775a7
blankled1924 <blankled1924@student.faytechcc.edu>
2026-04-05 20:52:41 -0500

    First python assignment
    taking input from user and printing output

---
+++ P1Lab1_BlankleyDonovan.py
@@ -0,0 +1,12 @@
+# Donovan Blankley
+# April 5, 2026
+# P1 Lab 1
+# input and printing
+
+first_name = input("enter your first name: ")
+last_name = input("enter your last name: ")
+
+print("Hello,",first_name,last_name + "!","Welcome to CTI-110")
+
+
+
